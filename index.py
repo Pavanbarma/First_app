fc4ed0a12afa3b3b3c9740c96a85da38a5271b10
Pavan barma <78493953+Pavanbarma@users.noreply.github.com>
2023-12-29 13:54:05 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,10 @@
-# n = int(input())
-# if n >= 0:
-#     if (n >= 30) and (n <= 50):
-#         print("Average")
-#     elif (n >= 51) and (n <= 60):
-#         print("Good")
-#     elif (n >= 61) and (n <= 80):
-#         print("Excellent")
-#     else:
-#         print("Outstanding")
-# else:
-#     print("Number should be graterthan Zero")
-    
-    
-n = int(input())
-a = list(map(int, input().split()))
-new = []
-for i in a:
-    if i > 0:
-        new.append(a.index(i))
-print(len(new))
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, this is a simple Flask API running on EC2!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
